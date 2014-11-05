5cd45944c055ee3d04c38050695999318b6f085a
Joel Purra <code+github@joelpurra.com>
2014-11-05 15:17:41 +0100

    Update document version generation for preamble.tex

--- generate-pdfs.sh
+++ generate-pdfs.sh
@@ -4,27 +4,29 @@ set -e
 [[ ! `which git` ]] && { echo "git is required" 1>&2; exit 1; }
 [[ ! `which lyx` ]] && { echo "lyx is required" 1>&2; exit 1; }
 
-deleteTmpLyx(){
-	find "$PWD" -name '*.tmp.lyx' -delete
+deleteGeneratedTmpFiles(){
+	find "$PWD" -name '.generated.version' -delete
 }
 
 touchedLyxFileBases=()
 
 deleteAllTmp(){
+	deleteGeneratedTmpFiles
+
 	local tmpFiles=()
 
 	for touchedLyxFileBase in "${touchedLyxFileBases[@]}"; do
-		for rmfile in "$touchedLyxFileBase".{aux,log,out,tex,nlo,toc,lof,lot,synctex,tmp.lyx,tmp.pdf}; do
+		for rmfile in "$touchedLyxFileBase".{aux,log,out,tex,nlo,toc,lof,lot,synctex}; do
 			[[ -s "$rmfile" ]] && tmpFiles+=("$rmfile");
 		done;
 	done;
 
-	rm "${tmpFiles[@]}"
+	(( "${#tmpFiles[@]}" > 0 )) && rm "${tmpFiles[@]}"
 }
 
 trap 'deleteAllTmp' EXIT
 
-deleteTmpLyx
+deleteGeneratedTmpFiles
 
 description=$(git describe --tags --match 'v[0-9]*' --always --dirty='-SNAPSHOT')
 logfile="$(basename "$BASH_SOURCE").log"
@@ -33,20 +35,18 @@ echo "Version $description"
 
 for lyx in $(find "$PWD" -name '*.lyx');
 do
-	folder="$(dirname '$lyx')"
+	folder="$(dirname "$lyx")"
 	base="${lyx%.lyx}"
 
 	echo -n "Generating $lyx ... "
 
-	cd "$folder"
+	pushd "$folder" > /dev/null
+	echo "$description" > ".generated.version"
 
-	# Hacky rewrite of .lyx file
-	cat "$base.lyx" | sed "s/(Unknown~document~version)/$description/" > "$base.tmp.lyx"
 	touchedLyxFileBases+=("$base")
-	lyx --force-overwrite --export pdf2 "$base.tmp.lyx" &> "$logfile"
-	mv "$base.tmp.pdf" "$base.pdf"
+	lyx --force-overwrite --export pdf2 "$lyx" &> "$logfile"
 
 	echo "done."
 
-	cd - > /dev/null
+	popd > /dev/null
 done
